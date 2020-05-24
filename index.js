fd1971e655252ce956885b233cfa7aa9a6c95ff1
katungi <dankatdennis@gmail.com>
2020-05-24 15:42:22 +0300

    checking if i remember node :alien:

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+* Primary files for the api
+
+*/
+
+const http = require('http');
+
+// the server should respond to all requests with a string
+
+const server  = http.createServer(function(req,res){
+    res.end("Hello world\n");
+});
+
+// start teh server and have it listen to port 3000
+
+server.listen(3000);
